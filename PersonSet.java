a09c8e5774b33260417c789f9ba3da4e38d23d06
Fielding <fieldingparsons@gmail.com>
2025-03-02 05:54:57 -0700

    finished up the for loop and the method

--- PersonSet.java
+++ PersonSet.java
@@ -38,8 +38,8 @@ public class PersonSet implements PersonList {
 	public String toString() {
 		String fullSet = "Name	Height (cm)		Weight (kg)";
 		for (int i = 0; i < personArrayList.size(); i++) {
-			
+			fullSet = fullSet+"\n"+personArrayList.get(i).toString();
 		}
-		return "";
+		return fullSet;
 	}
 }
