f099daa986076acca4253580c7882f6c57ae4abb
Fielding <fieldingparsons@gmail.com>
2025-03-02 19:41:23 -0700

    finished PersonImperialSet's add

--- PersonImperialSet.java
+++ PersonImperialSet.java
@@ -10,6 +10,7 @@ public class PersonImperialSet extends PersonSet {
 	@Override
 	public void add(Person JDoe) {
 		JDoe.setHeight(JDoe.getHeight() * 0.39); //https://www.rapidtables.com/convert/length/cm-to-inch.html
-		JDoe.setWeight(JDoe.getWeight() * 2.20); //https://www.rapidtables.com/convert/weight/kg-to-pound.htmls 2.20
+		JDoe.setWeight(JDoe.getWeight() * 2.20); //https://www.rapidtables.com/convert/weight/kg-to-pound.htmls
+		super.add(JDoe);
 	}
 }
