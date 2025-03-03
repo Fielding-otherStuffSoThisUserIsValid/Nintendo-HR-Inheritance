0a01a0f1134d45a0ba5964e3fecc6629a452861a
Fielding <fieldingparsons@gmail.com>
2025-03-02 19:43:39 -0700

    added a toString to PersonImperialSet

--- PersonImperialSet.java
+++ PersonImperialSet.java
@@ -13,4 +13,14 @@ public class PersonImperialSet extends PersonSet {
 		JDoe.setWeight(JDoe.getWeight() * 2.20); //https://www.rapidtables.com/convert/weight/kg-to-pound.htmls
 		super.add(JDoe);
 	}
+	
+	// This does the exact same thing as PersonSet's toString except it uses imperial units
+	@Override
+	public String toString() {
+		String fullSet = "Name	Height (in)		Weight (lbs)";
+		for (int i = 0; i < personArrayList.size(); i++) {
+			fullSet = fullSet+"\n"+personArrayList.get(i).toString();
+		}
+		return fullSet;
+	}
 }
