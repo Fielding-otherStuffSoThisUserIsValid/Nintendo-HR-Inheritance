1126aced1a0258dc0bc3479e058c99957272e6c9
Fielding <fieldingparsons@gmail.com>
2025-03-02 18:10:21 -0700

    added more to PersonOrderedSet's add

--- PersonOrderedSet.java
+++ PersonOrderedSet.java
@@ -5,10 +5,12 @@ March 2 2025
 Nintendo-HR-Inheritance
 CSCI 2251 R01
 */
+import java.util.Collections;
 public class PersonOrderedSet extends PersonSet {
 	
 	@Override
 	public void add(Person JDoe) {
 		super.add(JDoe);
+		Collections.sort(personArrayList);
 	}
 }
